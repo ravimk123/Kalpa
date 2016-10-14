47198f84ecc82feb3fc8a88be12287b8be9a9c8b
ravi <ravimk123@gmail.com>
2016-10-14 13:30:32 +0530

    combo box

--- Local_Project/Form1.Designer.vb
+++ Local_Project/Form1.Designer.vb
@@ -26,6 +26,7 @@ Partial Class Form1
         Me.ComboBox1 = New System.Windows.Forms.ComboBox
         Me.Button2 = New System.Windows.Forms.Button
         Me.CheckedListBox1 = New System.Windows.Forms.CheckedListBox
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox
         Me.SuspendLayout()
         '
         'Button1
@@ -57,16 +58,25 @@ Partial Class Form1
         'CheckedListBox1
         '
         Me.CheckedListBox1.FormattingEnabled = True
-        Me.CheckedListBox1.Location = New System.Drawing.Point(341, 39)
+        Me.CheckedListBox1.Location = New System.Drawing.Point(287, 84)
         Me.CheckedListBox1.Name = "CheckedListBox1"
         Me.CheckedListBox1.Size = New System.Drawing.Size(120, 94)
         Me.CheckedListBox1.TabIndex = 7
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(32, 99)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox2.TabIndex = 8
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(513, 262)
+        Me.Controls.Add(Me.ComboBox2)
         Me.Controls.Add(Me.CheckedListBox1)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.ComboBox1)
@@ -80,5 +90,6 @@ Partial Class Form1
     Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
     Friend WithEvents Button2 As System.Windows.Forms.Button
     Friend WithEvents CheckedListBox1 As System.Windows.Forms.CheckedListBox
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
 
 End Class
